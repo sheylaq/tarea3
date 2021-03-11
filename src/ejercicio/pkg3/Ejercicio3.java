06b9345d82c9d00ab511ebae7a154b777be93d35
sheylaq <80442391+sheylaq@users.noreply.github.com>
2021-03-10 23:36:41 -0600

    primer commit

---
+++ src/ejercicio/pkg3/Ejercicio3.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg3;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int M = 6, T = 1, K = -10;
+        
+        boolean x;
+        System.out.println("__________________________________________________________\n");
+        System.out.println("M > T   ? \n");
+        if(M > T){
+     x=true;
+    }
+        else{
+    x=false;}
+           System.out.println(x);
+           
+System.out.println("__________________________________________________________\n");
+System.out.println("T / K == -5  ? \n");
+    if((T / K) == -5){
+     x=true;
+    }
+        else{
+    x=false;}
+           System.out.println(x);
+System.out.println("__________________________________________________________\n");
+System.out.println("(M+T == 7) || (M-T == 5)  ? \n");
+    if(((M+T == 7) || (M-T == 5)) ){
+     x=true;
+    }
+        else{
+    x=false;}
+           System.out.println(x);           
+           
+           
+}}
